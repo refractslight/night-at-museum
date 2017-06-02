baf0d6af826052d018d60547998b71d68926b987
Susie <susie.odneal@gmail.com>
2017-06-02 17:20:05 -0500

    began painting audio

---
+++ Assets/Scripts/paintingAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paintingAudio : MonoBehaviour {
+
+	public bool isPlaying = false;
+	public GvrAudioSource painting;
+	public audio
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void playAudio(){
+		painting.PlayOneShot;
+		isPlaying = true;
+	}
+}
