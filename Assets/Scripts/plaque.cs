065a2d0a0680657995234d7166ee9f64e745ebc2
Susie <susie.odneal@gmail.com>
2017-06-16 14:31:13 -0500

    I forgot what I did here

---
+++ Assets/Scripts/plaque.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plaque : MonoBehaviour {
+
+	public GameObject plaqueObject;
+	public bool isHidden = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void Appear() {
+		gameObject.SetActive(true);
+		bool isHidden = false;
+	}
+
+	void Disappear() {
+		gameObject.SetActive (false);
+		bool isHidden = true;
+	}
+
+}
