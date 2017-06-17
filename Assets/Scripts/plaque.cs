b1c9cc5c6124b5dc19b6ee03e8ee0eece8b7670c
Susie <susie.odneal@gmail.com>
2017-06-17 11:43:21 -0500

    Added audio at each station

--- Assets/Scripts/plaque.cs
+++ Assets/Scripts/plaque.cs
@@ -7,11 +7,21 @@ public class plaque : MonoBehaviour {
 	public GameObject panel;
 	public GameObject dismissButton;
 	public GameObject learnMore;
+	public AudioClip paintingAudio;
+	public AudioSource panelBackground;
+	void Start() {
+		panelBackground = GetComponent<AudioSource> ();
+	}
+
 
 	public void togglePanel (GameObject panel){
 		panel.SetActive (!panel.activeSelf);
 		dismissButton.SetActive (!dismissButton.activeSelf);
 		learnMore.SetActive (!learnMore.activeSelf);
+
 	}
 
+	/*public void playAudio() {
+		panelBackground.PlayOneShot (paintingAudio);
+	}*/
 }
